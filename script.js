19ca390fb03f0e757cc673b7825fdcc60474a4cb
sam-martini <113351225+sam-martini@users.noreply.github.com>
2023-03-13 15:23:50 +1100

    reset

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
 // - - - - - Query Selectors - - - - - //
 
-const screenValue = document.querySelector('.screen');
-const numBtns = document.querySelectorAll('.num-btn');
-const opBtns = document.querySelectorAll('.op-btn');
-const equalsBtn = document.querySelector('.equals-btn');
-const allClearBtn = document.querySelector('.ac-btn');
 
 
 // - - - - - Global Variables - - - - - //
 
-let hasResult = false;
 
-
-
-
-let firstNum = null;
-let operation = null;
 
 // - - - - - Math / Operator Functions - - - - - //
 
@@ -33,62 +22,14 @@ function operate(a, b, operator) {
 
 // - - - - - Do Calculator Stuff - - - - - //
 
-function handleOperatorClick(operator) {
-    firstNum = parseInt(screenValue.innerText);
-    operation = operator;
-    clearDisplay();
-}
 
-function handleEqualsClick() {
-    const secondNum = parseInt(screenValue.innerText);
-    const result = operate(firstNum, secondNum, operation);
-    updateDisplay(result);
-    hasResult = true;
-}
 
 
 
 // - - - - - Display Functions - - - - - //
 
-function showValue(value) {
-    checkResult();
-    screenValue.innerText += value;
-}
 
-function updateDisplay(value) {
-    screenValue.innerText = value;
-}
-
-function clearDisplay() {
-    screenValue.innerText = '';
-}
-
-
-
-
-
-function checkResult() {
-    if (hasResult) {
-        hasResult = false;
-        clearDisplay();
-    }
-}
 
 
 // - - - - - Event Listeners - - - - - //
 
-numBtns.forEach(btn => {
-    btn.addEventListener('click', () => {
-        showValue(btn.value);
-    })
-});
-
-opBtns.forEach(btn => {
-    btn.addEventListener('click', () => {
-        handleOperatorClick(btn.value);
-    })
-})
-
-allClearBtn.addEventListener('click', clearDisplay);
-
-equalsBtn.addEventListener('click', handleEqualsClick);
