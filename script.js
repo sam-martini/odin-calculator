ffb5b04983bcdc58c0b4fca474c61f26014c63ef
sam-martini <113351225+sam-martini@users.noreply.github.com>
2023-03-13 16:43:13 +1100

    feat: Populate display from num buttons

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // - - - - - Query Selectors - - - - - //
 const numBtns = document.querySelectorAll('[data-number]');
+const decimalBtn = document.querySelector('[data-decimal]');
 const opBtns = document.querySelectorAll('[data-operation]');
 const equalsBtn = document.querySelector('[data-equals]');
 const clearBtn = document.querySelector('[data-clear]');
 const allClearBtn = document.querySelector('[data-all-clear]');
-const screenValue = document.querySelector('[data-screen-value]');
+const display = document.querySelector('[data-screen-display]');
 
 // - - - - - Global Variables - - - - - //
 
@@ -12,12 +13,25 @@ const screenValue = document.querySelector('[data-screen-value]');
 // - - - - - Calculator Object - - - - - //
 // Create a calculator object that keeps track of everything to construct a valid expression.
 const calculator = {
-    screenValue: '0',
+    screenValue: '',
     firstNum: null,
     waitingForSecondNum: false,
     operator: null,
 };
 
+
+
+function inputDigit(digit) {
+    calculator.screenValue += digit;
+    console.log(calculator);
+}
+
+function inputDecimal(decimal) {
+    if (!calculator.screenValue.includes(decimal)) {
+        calculator.screenValue += decimal;
+        console.log(calculator);
+    }
+}
 
 
 // - - - - - Math / Operator Functions - - - - - //
@@ -40,8 +54,24 @@ function operate(a, b, operator) {
 
 // - - - - - Display Functions - - - - - //
 
+function updateDisplay() {
+    display.textContent = calculator.screenValue;
+}
+
+
 
 
 
 // - - - - - Event Listeners - - - - - //
 
+numBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        inputDigit(btn.value);
+        updateDisplay();
+    })
+});
+
+decimalBtn.addEventListener('click', (e) => {
+    inputDecimal(e.target.value);
+    updateDisplay();
+})
